47ddebb845246110dce2b98ca78f8616bb532708
“hphan9” <emilyhuyenphan@gmail.com>
2022-01-25 16:47:40 -0500

    Adding index.js file , change main entry point to index.js

--- src/logger.js
+++ src/logger.js
@@ -4,7 +4,7 @@
 const options = { level: process.env.LOG_LEVEL || 'info' };
 
 // If we're doing `debug` logging, make the logs easier to read
-if (options.level === 'debug') {
+if (options.level === 'info') {
   // https://github.com/pinojs/pino-pretty
   options.transport = {
     target: 'pino-pretty',
