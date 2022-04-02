908a47d4c295b512313e99141677e512705edd44
“hphan9” <emilyhuyenphan@gmail.com>
2022-04-02 16:24:28 -0400

    fix cd pipeline in code refer to step login-ecr and eslint

--- src/routes/api/put.js
+++ src/routes/api/put.js
@@ -1,7 +1,7 @@
 // src/routes/api/get.js
 
-const { Fragment } = require('../../model/fragment');
-var { createSuccessResponse, createErrorResponse } = require('../../response');
+//const { Fragment } = require('../../model/fragment');
+var {  createErrorResponse } = require('../../response');
 const logger = require('../../logger');
 
 module.exports = async(req,res)=>{
