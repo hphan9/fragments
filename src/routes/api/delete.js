e871abaa6d4ca3782907fcde7c49cac0b55f51ea
“hphan9” <emilyhuyenphan@gmail.com>
2022-04-03 22:04:16 -0400

    add delete route

---
+++ src/routes/api/delete.js
@@ -0,0 +1,17 @@
+// src/routes/api/delete.js
+
+const { Fragment } = require('../../model/fragment');
+var { createSuccessResponse, createErrorResponse } = require('../../response');
+const logger = require('../../logger');
+
+module.exports = async (req, res) => {
+  const { id } = req.params;
+  logger.info({ id }, `Start handling Delete Fragment request `);
+  try {
+    await Fragment.delete(req.user, id);
+    return res.status(200).json(createSuccessResponse());
+  } catch (err) {
+    logger.warn(`Error deleting data for fragment ${err}`);
+    return res.status(404).json(createErrorResponse(404, 'Error deleting data for fragment'));
+  }
+};
---
+++ tests/unit/delete.test.js
@@ -0,0 +1,38 @@
+const request = require('supertest');
+const app = require('../../src/app');
+
+describe('DELETE /v1/fragments/id', () => {
+  // If the request is missing the Authorization header, it should be forbidden
+  test('unauthenticated requests are denied', () =>
+    request(app).delete('/v1/fragments/id').expect(401));
+
+  // If the wrong username/password pair are used (no such user), it should be forbidden
+  test('incorrect credentials are denied', () =>
+    request(app)
+      .delete('/v1/fragments/id')
+      .auth('invalid@email.com', 'incorrect_password')
+      .expect(401));
+
+  test('delete invalid fragments id should return 404', async () => {
+    const res = await request(app).delete('/v1/fragments/id').auth('user2@email.com', 'password2');
+    expect(res.statusCode).toBe(404);
+    expect(res.body.status).toBe('error');
+  });
+
+  // delete valid fragment should give a success result
+  test('authenticated users can delete valid fragment', async () => {
+    const data = 'Have a nice day';
+    //make post request
+    const resPost = await request(app)
+      .post('/v1/fragments')
+      .set('Content-type', 'text/plain')
+      .send(data)
+      .auth('user2@email.com', 'password2');
+    const fragmentTest = resPost.body.fragment;
+    //make delete request
+    const resDelete = await request(app)
+      .delete(`/v1/fragments/${fragmentTest.Id}`)
+      .auth('user2@email.com', 'password2');
+    expect(resDelete.statusCode).toBe(404);
+  });
+});
