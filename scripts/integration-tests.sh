0eda81ed0c6fcd38035c85d86e8e597e15ed2497
“hphan9” <emilyhuyenphan@gmail.com>
2022-04-03 22:53:36 -0400

    Update docker-compose.yml to have cognito variable to be forwarded from the env , add intergration test for delete route

--- scripts/integration-tests.sh
+++ scripts/integration-tests.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-# Run all .hurl files under tests/integration.
+# Run all .hurl files under tests/integration.do
 # Make sure hurl is installed, see https://hurl.dev
 hurl --test --glob "tests/integration/**/*.hurl"
